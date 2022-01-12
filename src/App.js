36bf046820380d80cf35bc143f2b7f492df2ba50
Alina Ananjeva <alina.ananjeva@eurodrivefinance.co.uk>
2022-01-12 12:09:48 +0000

    routing in app

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
 import React, { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import { ArticlesList } from "./components/ArticlesList";
 import { Header } from "./components/Header";
 import { Navbar } from "./components/Navbar";
+import { Article } from "./components/Article";
 
 function App() {
-  const [topic, setTopic] = useState("all");
   return (
     <div className="App">
-      <Header />
-      <Navbar setTopic={setTopic} />
-      <ArticlesList topic={topic} />
+      <BrowserRouter>
+        <Header />
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<ArticlesList />} />
+          <Route path="/articles" element={<ArticlesList />} />
+          <Route path="/articles/topic/:slug" element={<ArticlesList />} />
+          <Route path="/articles/:article_id" element={<Article />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
