1c6ac1b65e96deba0a696a45b54d3fcc559ecd19
Alina Ananjeva <alina.ananjeva@eurodrivefinance.co.uk>
2022-01-12 12:08:09 +0000

    removed routng to app.js

--- src/index.js
+++ src/index.js
@@ -2,17 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Article } from "./components/Article";
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="*" element={<App />} />
-        <Route path="/articles/:article_id" element={<Article />} />
-      </Routes>
-    </BrowserRouter>
+    <App />
   </React.StrictMode>,
   document.getElementById("root")
 );
