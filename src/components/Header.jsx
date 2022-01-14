699b94495e770aed3a03b08a1b3a81fad7acb132
Alina Ananjeva <alina.ananjeva@eurodrivefinance.co.uk>
2022-01-14 09:19:51 +0000

    header now clickable, leads to home page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,17 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 export const Header = () => {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate("/");
+  };
   return (
     <div className="header">
-      <h1>AA News Portal</h1>
+      <h1 className="main-header" onClick={handleClick}>
+        AA News Portal
+      </h1>
     </div>
   );
 };
